6b976fed06097c2fdea9e1976a5b516f78ebd4e9
Shaneann01 <shaneann.ahtack@vodafone.com>
2023-11-20 15:04:51 +0000

    Added Add item, update item and delete item

--- server/routes/Item.js
+++ server/routes/Item.js
@@ -22,4 +22,56 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
+router.post("/", async (req, res, next) => {
+  try {
+    const { name, description, category, price, image } = req.body;
+
+    const newItem = await Item.create({
+      name,
+      description,
+      category,
+      price,
+      image,
+    });
+
+    res.json(newItem);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const { name, description, category, price, image } = req.body;
+    const item = await Item.findByPk(id);
+
+    item.name = name;
+    item.description = description;
+    item.category = category;
+    item.price = price;
+    item.image = image;
+
+    await item.save();
+
+    res.send(item);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const deleteId = req.params.id;
+    const deletedItem = await Item.destroy({
+      where: {
+        id: deleteId,
+      },
+    });
+
+    res.json({ message: `Item has been successfully deleted` });
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = router;
