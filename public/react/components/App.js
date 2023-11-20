7aacebcedde2fe5a37ae9bc23805044f0da971cb
JoeCaswall <joe.caswall@gmail.com>
2023-11-20 15:07:35 +0000

    add page form completed

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { ItemsList } from "./ItemsList";
+import { AddItem } from "./AddItem";
 
 // import and prepend the api url to any fetch calls
 import apiURL from "../api";
 
 export const App = () => {
   const [items, setItems] = useState([]);
+
+  const [isAddPage, setIsAddPage] = useState(false);
 
   async function fetchItems() {
     try {
@@ -27,6 +30,7 @@ export const App = () => {
       <h1>Items</h1>
       <h2>All things</h2>
       <ItemsList items={items} />
+      <AddItem fetchItems={fetchItems} setIsAddPage={setIsAddPage} />
     </main>
   );
 };
