2a9da9dc135a03eccfcc78f5797c53b485ae7942
JoeCaswall <joe.caswall@gmail.com>
2023-11-20 11:43:57 +0000

    Model added

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,21 @@
-const {Sequelize} = require('sequelize')
-const {sequelize} = require('../db')
+const { Sequelize } = require("sequelize");
+const { sequelize } = require("../db");
 
 const Sauce = sequelize.define("sauces", {
   name: Sequelize.STRING,
   image: Sequelize.STRING,
 });
 
+const Item = sequelize.define("items", {
+  name: Sequelize.STRING,
+  description: Sequelize.STRING,
+  category: Sequelize.STRING,
+  price: Sequelize.INTEGER,
+  image: Sequelize.STRING,
+});
+
 module.exports = {
   db: sequelize,
   Sauce,
+  Item,
 };
