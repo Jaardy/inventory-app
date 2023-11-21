937ed1ed92211a6941679a66709bd7b03990418f
JoeCaswall <joe.caswall@gmail.com>
2023-11-21 14:41:51 +0000

    brought in item tests

--- server/routes/Item.test.js
+++ server/routes/Item.test.js
@@ -1,23 +1,48 @@
 const { describe, it, expect, beforeAll, afterAll } = require("@jest/globals");
 const { Item } = require("../models");
 const { sequelize } = require("../db");
-const { seed } = require("../seed");
+
+const { items } = require("../seedData");
+const request = require("supertest");
+const app = require("../app");
 
 //clear db and reseed before all tests
 beforeAll(async () => {
-  await seed();
+  await sequelize.sync({ force: true });
+  await Item.bulkCreate(items);
 });
 
-afterAll(async () => {
-  await seed();
+describe("GET /items/ unit tests", () => {
+  it("/ endpoint works", async () => {
+    const response = await request(app).get("/api/items/");
+    expect(response.statusCode).toBe(200);
+  });
 });
 
-describe("GET /items/ unit tests", () => {});
+describe("GET /items/:id unit tests", () => {
+  it("/:id enpoint works", async () => {
+    const response = await request(app).get("/api/items/1");
+    expect(response.statusCode).toBe(200);
+  });
+});
 
-describe("GET /items/:id unit tests", () => {});
+describe("POST /items/ unit tests", () => {
+  it("/ Post endpoint works", async () => {
+    const response = await request(app).post("/api/items/");
+    expect(response.statusCode).toBe(200);
+  });
+});
 
-describe("POST /items/ unit tests", () => {});
+describe("PUT /items/:id unit tests", () => {
+  it("/ endpoint for Put is working", async () => {
+    const response = await request(app).put("/api/items/1");
+    expect(response.statusCode).toBe(200);
+  });
+});
 
-describe("PUT /items/:id unit tests", () => {});
-
-describe("DELETE /items/:id unit tests", () => {});
+describe("DELETE /items/:id unit tests", () => {
+  it("/ endpoint for delete is working", async () => {
+    const response = await request(app).delete("/api/items/1");
+    expect(response.statusCode).toBe(200);
+  });
+});
