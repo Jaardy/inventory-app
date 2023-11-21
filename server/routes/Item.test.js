65bb2ce63268c7f22cb02112a8bb7bb822401362
JoeCaswall <joe.caswall@gmail.com>
2023-11-21 10:14:45 +0000

    test writing template

---
+++ server/routes/Item.test.js
@@ -0,0 +1,23 @@
+const { describe, it, expect, beforeAll, afterAll } = require("@jest/globals");
+const { Item } = require("../models");
+const { sequelize } = require("../db");
+import seed from "../seed";
+
+//clear db and reseed before all tests
+beforeAll(async () => {
+  seed();
+});
+
+afterAll(async () => {
+  seed();
+});
+
+describe("GET /items/ unit tests", () => {});
+
+describe("GET /items/:id unit tests", () => {});
+
+describe("POST /items/ unit tests", () => {});
+
+describe("PUT /items/:id unit tests", () => {});
+
+describe("DELETE /items/:id unit tests", () => {});
