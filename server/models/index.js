18eb941138accaa58be5664ba18d87153f811f43
Laila Case <lai_case@hotmail.com>
2023-11-21 14:29:10 +0000

    add associations

--- server/models/index.js
+++ server/models/index.js
@@ -4,6 +4,12 @@ const Item = require("./Item");
 const User = require("./User");
 const Order = require("./Order");
 
+User.hasOne(Order);
+Order.belongsTo(User);
+
+Order.hasMany(Item);
+Item.belongsTo(Order);
+
 module.exports = {
   db: sequelize,
   Item,
