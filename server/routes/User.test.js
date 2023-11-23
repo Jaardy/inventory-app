333d771cbc7dac791ae2d5101f43afc510e5ae3a
JoeCaswall <joe.caswall@gmail.com>
2023-11-23 10:26:10 +0000

    user tests written

--- server/routes/User.test.js
+++ server/routes/User.test.js
@@ -1,12 +1,27 @@
 const express = require("express");
-const { User } = require("../models");
 const request = require("supertest");
-const { describe, it, expect, beforeAll, afterAll } = require("@jest/globals");
+const {
+  describe,
+  it,
+  expect,
+  beforeEach,
+  afterEach,
+} = require("@jest/globals");
 const app = require("../app");
 const seed = require("../seed");
+const seedData = require("../seedData");
 const { sequelize } = require("../db");
+app.use(express.json());
+app.use(express.urlencoded());
 
-beforeAll(async () => {
+const seedUsers = seedData.users;
+
+beforeEach(async () => {
+  await sequelize.sync({ force: true });
+  await await seed();
+});
+
+afterEach(async () => {
   await sequelize.sync({ force: true });
   await seed();
 });
@@ -16,23 +31,61 @@ describe("GET /users/ endpoint", () => {
     const res = await request(app).get("/api/users/");
     expect(res.statusCode).toBe(200);
   });
+  it("fetches all users", async () => {
+    const res = await request(app).get("/api/users");
+    const data = JSON.parse(res.text);
+    //expect correct amount of users
+    expect(data.length).toBe(seedUsers.length);
+    //expect all seed users to be in api
+    for (x in data) {
+      expect([data[x].username, data[x].email, data[x].password]).toEqual([
+        seedUsers[x].username,
+        seedUsers[x].email,
+        seedUsers[x].password,
+      ]);
+    }
+  });
 });
 
 describe("GET /users/:id endpoint", () => {
   it("reaches endpoint successfully", async () => {
-    const res = await request(app).get("/api/users/1");
+    const res = await request(app).get("/api/users/joecaswall");
     expect(res.statusCode).toBe(200);
+  });
+  it("fetches the correct user", async () => {
+    const res = await request(app).get("/api/users/joecaswall");
+    const data = JSON.parse(res.text);
+
+    expect([data.username, data.email, data.password]).toEqual([
+      seedUsers[0].username,
+      seedUsers[0].email,
+      seedUsers[0].password,
+    ]);
   });
 });
 
 describe("POST /users/ endpoint", () => {
   it("reaches endpoint successfully", async () => {
     const res = await request(app).post("/api/users/").send({
-      username: "test item",
+      username: "testuser",
       email: "test.test@test.test",
       password: "testy",
     });
     expect(res.statusCode).toBe(200);
+  });
+  it("successfully creates a user", async () => {
+    const res = await request(app).post("/api/users/").send({
+      username: "testuser",
+      email: "test.test@test.test",
+      password: "testy",
+    });
+    const user = await request(app).get("/api/users/testuser");
+    const userData = JSON.parse(user.text);
+    expect([userData.username, userData.email, userData.password]).toEqual([
+      "testuser",
+      "test.test@test.test",
+      "testy",
+    ]);
   });
 });
 
@@ -49,7 +102,21 @@ describe("PUT /users/:id endpoint", () => {
 
 describe("DELETE /users/:id endpoint", () => {
   it("reaches endpoint successfully", async () => {
-    const res = await request(app).delete("/api/users/1");
+    const res = await request(app).delete("/api/users/joecaswall");
     expect(res.statusCode).toBe(200);
+  });
+  it("deletes correct user", async () => {
+    await request(app).delete("/api/users/joecaswall");
+    const allUsers = await request(app).get("/api/users");
+    const allUsersData = JSON.parse(allUsers.text);
+    expect([
+      allUsersData[0].username,
+      allUsersData[0].email,
+      allUsersData[0].password,
+    ]).toEqual([
+      seedUsers[1].username,
+      seedUsers[1].email,
+      seedUsers[1].password,
+    ]);
   });
 });
